259f5f972ff28d237cc41a2065c01bef700c0051
wesleymp <wesleyxd@hotmail.com>
2022-01-25 09:47:05 -0300

    Implementação de cors e outras configurações

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
 const router = require('./routes/router');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
 
